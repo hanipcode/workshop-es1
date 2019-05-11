e18866c9bf8b2e5500b50218afb68d490e7c5207
Muhammad Hanif <hanifeij@yahoo.co.id>
2019-05-11 20:29:05 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("test another build");
+console.log("test another");
+
+function penjumlahan(a, b) {
+  return a + b;
+}
+
+function kuadrat(a) {
+  return a * a;
+}
+
+const filterNegatif = [1, 2, -3, 4].filter((nilai, index) => {
+  console.log(nilai, index);
+  return nilai > 0;
+});
+
+class Mathematic {
+  x = 5;
+  y = 6;
+  arrayKu = [1, 2, 3];
+}
+
+const mat = new Mathematic();
+
+console.log(mat.arrayKu);
